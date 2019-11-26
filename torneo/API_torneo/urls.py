8732f4cfc6673cae4b6f9077e8571662b6151313
ledin <ledinalvarado18@gmail.com>
2019-11-26 11:17:02 -0600

    creacion de serializers,url y views para Ciudad,Estadio,Equipo y Arbitro,

--- torneo/API_torneo/urls.py
+++ torneo/API_torneo/urls.py
@@ -6,7 +6,10 @@ router = routers.DefaultRouter()
 router.register('jugadores', views.ViewJugador)
 router.register('temporadas', views.ViewTemporada)
 router.register('posiciones-jugador', views.ViewPosicionJugador)
-
+router.register('ciudad',views.ViewCiudad)
+router.register('estadio',views.ViewEstadio)
+router.register('equipo',views.ViewEquipo)
+router.register('arbitro',views.ViewArbitro)
 urlpatterns = [
     path('', include(router.urls))
 ]
