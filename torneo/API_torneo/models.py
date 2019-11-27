0fa78a2520395b780f82b85e92cdd4f9cd225373
kebelandrade <andradekebel97@gmail.com>
2019-11-27 13:28:56 -0600

    Prueba

--- torneo/API_torneo/models.py
+++ torneo/API_torneo/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.db import models
 from rest_framework.exceptions import ValidationError
-from rest_framework import serializers
 
 
 class ModeloBase(models.Model):
