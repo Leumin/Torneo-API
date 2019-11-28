3a5f17ed3ccb7be9f100cc447edacdd320b5e2cc
leumin <leotroche20@gmail.com>
2019-11-28 12:11:28 -0600

    - se creo una carpeta para las views, y se cambiaron las rutas de los imports

--- torneo/API_torneo/urls.py
+++ torneo/API_torneo/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path, include
 
-from .views import ViewCrearUsuario
-from . import views
+from API_torneo.views.views_generales import ViewCrearUsuario
+from API_torneo.views import views_generales
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('jugadores', views.ViewJugador)
-router.register('temporadas', views.ViewTemporada)
-router.register('posiciones-jugador', views.ViewPosicionJugador)
-router.register('ciudad',views.ViewCiudad)
-router.register('estadio',views.ViewEstadio)
-router.register('equipo',views.ViewEquipo)
-router.register('arbitro',views.ViewArbitro)
+router.register('jugadores', views_generales.ViewJugador)
+router.register('temporadas', views_generales.ViewTemporada)
+router.register('posiciones-jugador', views_generales.ViewPosicionJugador)
+router.register('ciudad', views_generales.ViewCiudad)
+router.register('estadio', views_generales.ViewEstadio)
+router.register('equipo', views_generales.ViewEquipo)
+router.register('arbitro', views_generales.ViewArbitro)
 urlpatterns = [
     path('', include(router.urls)),
     path('usuarios/', ViewCrearUsuario.as_view(), name='crear_usuario')
