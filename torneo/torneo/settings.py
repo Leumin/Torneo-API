34bada472384a81c6f243dc2fa417e921158404f
leumin <leotroche20@gmail.com>
2019-12-05 19:57:37 -0600

    - update cors settings

--- torneo/torneo/settings.py
+++ torneo/torneo/settings.py
@@ -56,10 +56,6 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = (
-    'http://0.0.0.0:8080',
-    'http://0.0.0.0:8000',
-)
 
 
 AUTH_USER_MODEL = 'API_torneo.Usuario'
