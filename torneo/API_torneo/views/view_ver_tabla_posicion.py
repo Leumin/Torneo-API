e45859a13be6c561db229f3e51fee01b9c058644
leumin <leotroche20@gmail.com>
2019-12-09 15:23:31 -0600

    Revert "Views para estadística de partidos/goles"
    This reverts commit a0383974

--- torneo/API_torneo/views/view_ver_tabla_posicion.py
+++ torneo/API_torneo/views/view_ver_tabla_posicion.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
-
-from rest_framework import status
-from rest_framework.response import Response
+from rest_framework import viewsets, generics
 from rest_framework.views import APIView
 from API_torneo.serializers import *
 from API_torneo.models import *
 
 
-class Resultado(APIView):
-    pass
+class ViewVerTablaPosicion(APIView):
 
 
-class Resultados1(APIView):
-    pass
-
-
-class TablaPosicion(APIView):
-    pass
+    def get(self, request, format=None):
+        pass
