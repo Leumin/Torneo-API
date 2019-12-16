a691d9e92e1be997f949b536a55c4304c956fa55
kebelandrade <andradekebel97@gmail.com>
2019-12-15 21:39:39 -0600

    Correción en la view_goleadores.py.

--- torneo/API_torneo/views/view_goleadores.py
+++ torneo/API_torneo/views/view_goleadores.py
@@ -12,11 +12,11 @@ class ViewGoleadores(APIView):
 
     def get(self, request):
         data = []
-        goles = Goles.objects.values('jugador__jugador__nombre', 'jugador__equipo__nombre').annotate(
+        goles = Goles.objects.values('jugador__nombre', 'equipo__nombre').annotate(
             goles=Count('jugador'))
         for contenido in goles:
-            data.append({'jugador': contenido['jugador__jugador__nombre'],
-                         'equipo': contenido['jugador__equipo__nombre'],
+            data.append({'jugador': contenido['jugador__nombre'],
+                         'equipo': contenido['equipo__nombre'],
                          'goles': contenido['goles']})
         serializers = GoleadoresVerSerializer(data=data, many=True)
         if serializers.is_valid():
